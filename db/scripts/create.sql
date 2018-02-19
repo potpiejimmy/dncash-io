ce01f44aa149b15b4151a16d3961ae1e57cd569e
Thorsten Liese <thorsten.liese@dieboldnixdorf.com>
2018-02-19 23:38:36 +0100

    BCSDM-5: Start creating DB model

---
+++ db/scripts/create.sql
@@ -0,0 +1,22 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+
+-- -----------------------------------------------------
+-- Table `customer`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `customer` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(128) NOT NULL,
+  `password` VARCHAR(128) NOT NULL,
+  `display_name` VARCHAR(128) NULL,
+  `roles` VARCHAR(255) NOT NULL DEFAULT 'user',
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
