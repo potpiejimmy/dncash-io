d005b9fb0c8c23bcf741be0562f41851dd646c36
Thorsten Liese <thorsten.liese@dieboldnixdorf.com>
2018-03-04 08:49:16 +0100

    BCSDM-5: implemented PKCS#1 padding for increased token security (no storing in DB, compare encoded token like signatures)

--- business/token.ts
+++ business/token.ts
@@ -1,8 +1,11 @@
 import * as db from "../util/db";
 import * as Device from "./device";
 import * as crypto from 'crypto';
+import * as constants from 'constants';
+import * as forge from 'node-forge';
 import * as uuid from "uuid/v4"; // Random-based UUID
 import * as config from '../config';
+import { tokenChangeNotifier } from "../util/notifier";
 
 export function createToken(customer: any, token: any): Promise<any> {
     return Device.findByCustomerAndUUID(customer, token.device_uuid).then(device => {
@@ -20,8 +23,9 @@ export function createToken(customer: any, token: any): Promise<any> {
             token.owner_id = customer.id;
             token.owner_device_id = device.id;
             let code = crypto.randomBytes(config.DEFAULT_CODE_LEN);
-            token.secure_code = crypto.publicEncrypt(device.pubkey, code).toString('base64');
+            token.secure_code = encryptTokenCode(device.pubkey, code);
             token.info = JSON.stringify(token.info); // save info data as string
+            tokenChangeNotifier.notifyObservers(customer.id);
             return insertNew(token).then(id => findById(id)).then(t => exportToken(t));
         });
     });
@@ -41,6 +45,7 @@ export function findByCustomer(customer: any): Promise<any> {
 export function deleteByDeviceAndUUID(customer: any, device_uuid: string, uid: string): Promise<any> {
     return Device.findByCustomerAndUUID(customer, device_uuid).then(device => {
         if (!device) return;
+        tokenChangeNotifier.notifyObservers(customer.id);
         return db.querySingle("update token set state='DELETED' where owner_device_id=? and uuid=?", [device.id, uid]);
     });
 }
@@ -64,17 +69,43 @@ export function verifyAndLock(customer: any, device_uuid: string, radio_code: st
 
             // fetch the token device with its associated public key:
             return Device.findById(token.owner_device_id).then(tokenDevice => {
-                if (token.secure_code != crypto.publicEncrypt(tokenDevice.pubkey, code).toString('base64')) throw "Invalid token code.";
+                // compare the encrypted token codes:
+                if (token.secure_code != encryptTokenCode(tokenDevice.pubkey, code)) throw "Invalid token code.";
 
                 // okay, verified, now try to lock the token:
                 return atomicLockToken(token.id).then(success => {
                     if (!success) throw "Token not in open state.";
-                    // TODO: ournalize / connect for clearing
-                    return token;
+
+                    tokenChangeNotifier.notifyObservers(token.owner_id);
+                    // TODO: journalize / connect for clearing
+                    return exportToken(token);
                 })
             })
         });
     });
+}
+
+/**
+ * Returns the public key encrypted token using PKCS1_PADDING 
+ * @param pubkey PEM encoded public key
+ * @param code a variable size buffer with random data
+ */
+function encryptTokenCode(pubkey: string, code: Buffer): string {
+    // apply non-random PKCS#1-padding so we don't have to
+    // store the plain code in the DB and can just compare
+    // the encrypted code on token verification
+
+    // determine key length:
+    let keylen = forge.pki.publicKeyFromPem(pubkey).n.toString(16).length/2;
+
+    // Apply PKCS#1 padding:
+    let padded = Buffer.from([0x00, 0x02, ...new Array(keylen-code.length-3).fill(0xff), 0x00, ...code]);
+
+    // and encrypt using public key:
+    return crypto.publicEncrypt({
+        key: pubkey,
+        padding: constants.RSA_NO_PADDING /* already padded */
+    }, padded).toString('base64');
 }
 
 function exportTokens(tokens: Array<any>): Array<any> {
@@ -104,5 +135,5 @@ function findByUUID(uid: string): Promise<any> {
 }
 
 function atomicLockToken(id: number): Promise<boolean> {
-    return db.querySingle("update token set state='LOCKED' where id=? and state='OPEN'").then(res => res.affectedRows);
+    return db.querySingle("update token set state='LOCKED' where id=? and state='OPEN'",[id]).then(res => res.affectedRows);
 }
