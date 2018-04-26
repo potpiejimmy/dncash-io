cc915c8fb3ee9fbf83200a6f968e6c941ccd46af
Thorsten Liese <thorsten.liese@dieboldnixdorf.com>
2018-04-26 22:03:48 +0200

    BCSDM-5: completed documenting DB model

--- db/scripts/create.sql
+++ db/scripts/create.sql
@@ -12,32 +12,33 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- Table `customer`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `customer` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `created` TIMESTAMP NOT NULL DEFAULT NOW(),
-  `email` VARCHAR(128) NOT NULL,
-  `password` VARCHAR(128) NOT NULL,
-  `display_name` VARCHAR(128) NULL,
-  `roles` VARCHAR(255) NOT NULL DEFAULT 'user',
-  `twofasecret` VARCHAR(32) NULL,
-  `status` TINYINT NOT NULL DEFAULT 0,
-  `info` VARCHAR(255) NULL,
+  `id` INT NOT NULL AUTO_INCREMENT COMMENT 'auto-generated ID',
+  `created` TIMESTAMP NOT NULL DEFAULT NOW() COMMENT 'time of creation',
+  `email` VARCHAR(128) NOT NULL COMMENT 'email address, used as log-on name',
+  `password` VARCHAR(128) NOT NULL COMMENT 'hex-encoded SHA256 password hash',
+  `display_name` VARCHAR(128) NULL COMMENT 'optional display name / full name',
+  `roles` VARCHAR(255) NOT NULL DEFAULT 'user' COMMENT 'comma-separated list of role names',
+  `twofasecret` VARCHAR(32) NULL COMMENT 'if this 2FA secret code is set, 2FA is enabled',
+  `status` TINYINT NOT NULL DEFAULT 0 COMMENT 'customer status, default 0, currently not used',
+  `info` VARCHAR(255) NULL COMMENT 'additional info data, JSON encoded',
   PRIMARY KEY (`id`),
   UNIQUE INDEX `email_UNIQUE` (`email` ASC))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+COMMENT = 'holds all customers';
 
 
 -- -----------------------------------------------------
 -- Table `customer_access`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `customer_access` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `customer_id` INT NOT NULL,
-  `created` TIMESTAMP NOT NULL DEFAULT NOW(),
-  `scope` VARCHAR(45) NOT NULL,
-  `apikey` VARCHAR(64) NOT NULL,
-  `apisecret` VARCHAR(64) NOT NULL,
-  `refname` VARCHAR(36) NULL,
-  `info` VARCHAR(255) NULL,
+  `id` INT NOT NULL AUTO_INCREMENT COMMENT 'auto-generated ID',
+  `customer_id` INT NOT NULL COMMENT 'the customer this API access belongs to',
+  `created` TIMESTAMP NOT NULL DEFAULT NOW() COMMENT 'time of creation',
+  `scope` VARCHAR(45) NOT NULL COMMENT 'API scope, for instance api-token or api-cash',
+  `apikey` VARCHAR(64) NOT NULL COMMENT 'API key random code',
+  `apisecret` VARCHAR(64) NOT NULL COMMENT 'API secret random code',
+  `refname` VARCHAR(36) NULL COMMENT 'a custom reference name, currently not used',
+  `info` VARCHAR(255) NULL COMMENT 'additional info data, JSON encoded',
   PRIMARY KEY (`id`),
   INDEX `fk_customer_access_customer_idx` (`customer_id` ASC),
   UNIQUE INDEX `apikey_UNIQUE` (`apikey` ASC),
@@ -46,24 +47,25 @@ CREATE TABLE IF NOT EXISTS `customer_access` (
     REFERENCES `customer` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+COMMENT = 'holds all API access credentials for customers';
 
 
 -- -----------------------------------------------------
 -- Table `customer_device`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `customer_device` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `customer_id` INT NOT NULL,
-  `uuid` VARCHAR(36) NOT NULL,
-  `created` TIMESTAMP NOT NULL DEFAULT NOW(),
-  `type` ENUM('MOBILE', 'ATM', 'CASH_REGISTER', 'OTHER') NOT NULL DEFAULT 'MOBILE',
-  `pubkey` VARCHAR(1024) NULL,
-  `algorithm` VARCHAR(45) NULL,
-  `lat` DECIMAL(10,8) NULL,
-  `lon` DECIMAL(11,8) NULL,
-  `refname` VARCHAR(36) NULL,
-  `info` VARCHAR(255) NULL,
+  `id` INT NOT NULL AUTO_INCREMENT COMMENT 'auto-generated ID',
+  `customer_id` INT NOT NULL COMMENT 'the customer this device belongs to',
+  `uuid` VARCHAR(36) NOT NULL COMMENT 'the device’s UUID',
+  `created` TIMESTAMP NOT NULL DEFAULT NOW() COMMENT 'time of device registration',
+  `type` ENUM('MOBILE', 'ATM', 'CASH_REGISTER', 'OTHER') NOT NULL DEFAULT 'MOBILE' COMMENT 'device type',
+  `pubkey` VARCHAR(1024) NULL COMMENT 'the device’s public key, usually only set for token devices (smartphones), PEM-encoded RSA public key',
+  `algorithm` VARCHAR(45) NULL COMMENT 'public key algorithm, currently not used',
+  `lat` DECIMAL(10,8) NULL COMMENT 'geo coordinates latitude',
+  `lon` DECIMAL(11,8) NULL COMMENT 'geo coordinates longitude',
+  `refname` VARCHAR(36) NULL COMMENT 'customer device reference name, set by the customer during registration',
+  `info` VARCHAR(255) NULL COMMENT 'additional custom data, JSON-encoded',
   PRIMARY KEY (`id`),
   INDEX `fk_customer_device_customer1_idx` (`customer_id` ASC),
   UNIQUE INDEX `uuid_UNIQUE` (`uuid` ASC),
@@ -72,31 +74,32 @@ CREATE TABLE IF NOT EXISTS `customer_device` (
     REFERENCES `customer` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+COMMENT = 'holds all customer devices, such as end-user smartphones (token devices) for banks and cash devices for banks and retail stores';
 
 
 -- -----------------------------------------------------
 -- Table `token`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `token` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `owner_id` INT NOT NULL,
-  `owner_device_id` INT NOT NULL,
-  `lock_device_id` INT NULL,
-  `uuid` VARCHAR(36) NOT NULL,
-  `created` TIMESTAMP NOT NULL DEFAULT NOW(),
-  `updated` TIMESTAMP NULL,
-  `type` ENUM('CASHOUT', 'CASHIN', 'PAYMENT', 'OTHER') NOT NULL DEFAULT 'CASHOUT',
-  `code_type` TINYINT NOT NULL DEFAULT 0,
-  `plain_code` VARCHAR(32) NULL,
-  `secure_code` VARCHAR(1024) NOT NULL,
-  `amount` DECIMAL(13,4) NOT NULL,
-  `symbol` VARCHAR(5) NOT NULL,
-  `state` ENUM('OPEN', 'DELETED', 'LOCKED', 'COMPLETED', 'FAILED', 'EXPIRED', 'CLEARED', 'CANCELED', 'REJECTED', 'RETRACTED') NOT NULL DEFAULT 'OPEN',
-  `clearstate` TINYINT NOT NULL DEFAULT 0,
-  `expires` TIMESTAMP NULL,
-  `refname` VARCHAR(36) NULL,
-  `info` VARCHAR(255) NULL,
+  `id` INT NOT NULL AUTO_INCREMENT COMMENT 'auto-generated ID',
+  `owner_id` INT NOT NULL COMMENT 'the token’s owner is the customer that created the token',
+  `owner_device_id` INT NOT NULL COMMENT 'the customer device that the token was created for - the secure_code of the token will be encrypted using that device’s public key',
+  `lock_device_id` INT NULL COMMENT 'the cash device that locked the token. this field will be set as soon as a token is claimed by a cash device and will not change from then on.',
+  `uuid` VARCHAR(36) NOT NULL COMMENT 'the unique token UUID used for all-time token identification',
+  `created` TIMESTAMP NOT NULL DEFAULT NOW() COMMENT 'time of creation',
+  `updated` TIMESTAMP NULL COMMENT 'timestamp of last token update',
+  `type` ENUM('CASHOUT', 'CASHIN', 'PAYMENT', 'OTHER') NOT NULL DEFAULT 'CASHOUT' COMMENT 'token type',
+  `code_type` TINYINT NOT NULL DEFAULT 0 COMMENT 'type of code, currently always 0, not used.',
+  `plain_code` VARCHAR(32) NULL COMMENT 'the optional system-wide unique plain code (used as short identification codes). will only be set if USE_PLAIN_CODES is configured and only during the OPEN phase of a token, otherwise this field is NULL.',
+  `secure_code` VARCHAR(1024) NOT NULL COMMENT 'random data encoded with the owner device’s public key, Base64-encoded',
+  `amount` DECIMAL(13,4) NOT NULL COMMENT 'the amount in whole integer units, e.g. cents.',
+  `symbol` VARCHAR(5) NOT NULL COMMENT 'the currency symbol, e.g. EUR',
+  `state` ENUM('OPEN', 'DELETED', 'LOCKED', 'COMPLETED', 'FAILED', 'EXPIRED', 'CLEARED', 'CANCELED', 'REJECTED', 'RETRACTED') NOT NULL DEFAULT 'OPEN' COMMENT 'the token state',
+  `clearstate` TINYINT NOT NULL DEFAULT 0 COMMENT 'a custom token clearing state, may be updated by the token owner through the Token API',
+  `expires` TIMESTAMP NULL COMMENT 'optional time of expiration',
+  `refname` VARCHAR(36) NULL COMMENT 'a custom reference name, usually set be the token owner on creation',
+  `info` VARCHAR(255) NULL COMMENT 'custom additional data, JSON-encoded. also holds the optional denomination data.',
   PRIMARY KEY (`id`),
   INDEX `fk_token_customer1_idx` (`owner_id` ASC),
   INDEX `fk_token_customer_device1_idx` (`owner_device_id` ASC),
@@ -118,19 +121,20 @@ CREATE TABLE IF NOT EXISTS `token` (
     REFERENCES `customer_device` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+COMMENT = 'the central cash token table';
 
 
 -- -----------------------------------------------------
 -- Table `journal`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `journal` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `customer_id` INT NOT NULL,
-  `created` TIMESTAMP NOT NULL DEFAULT NOW(),
-  `entity` VARCHAR(45) NOT NULL,
-  `action` VARCHAR(45) NOT NULL,
-  `data` TEXT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT COMMENT 'auto-generated ID',
+  `customer_id` INT NOT NULL COMMENT 'the customer the action is associated with',
+  `created` TIMESTAMP NOT NULL DEFAULT NOW() COMMENT 'time of creation',
+  `entity` VARCHAR(45) NOT NULL COMMENT 'the entity this journal entry is referring to, e.g. token',
+  `action` VARCHAR(45) NOT NULL COMMENT 'action type identifier',
+  `data` TEXT NULL COMMENT 'JSON-encoded journal data',
   PRIMARY KEY (`id`),
   INDEX `fk_journal_customer1_idx` (`customer_id` ASC),
   CONSTRAINT `fk_journal_customer1`
@@ -138,18 +142,19 @@ CREATE TABLE IF NOT EXISTS `journal` (
     REFERENCES `customer` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+COMMENT = 'system journal';
 
 
 -- -----------------------------------------------------
 -- Table `customer_param`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `customer_param` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `customer_id` INT NOT NULL,
-  `pkey` VARCHAR(45) NOT NULL,
-  `pvalue` VARCHAR(1024) NULL,
-  `updated` TIMESTAMP NOT NULL DEFAULT NOW(),
+  `id` INT NOT NULL AUTO_INCREMENT COMMENT 'auto-generated ID',
+  `customer_id` INT NOT NULL COMMENT 'the customer this param belongs to',
+  `pkey` VARCHAR(45) NOT NULL COMMENT 'parameter key',
+  `pvalue` VARCHAR(1024) NULL COMMENT 'parameter value',
+  `updated` TIMESTAMP NOT NULL DEFAULT NOW() COMMENT 'timestamp of last update or creation',
   PRIMARY KEY (`id`),
   INDEX `fk_customer_param_customer1_idx` (`customer_id` ASC),
   UNIQUE INDEX `pkey_customer_UNIQUE` (`customer_id` ASC, `pkey` ASC),
@@ -158,22 +163,23 @@ CREATE TABLE IF NOT EXISTS `customer_param` (
     REFERENCES `customer` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+COMMENT = 'holds customer specific parameters';
 
 
 -- -----------------------------------------------------
 -- Table `clearing`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `clearing` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `token_id` INT NOT NULL,
-  `debitor_id` INT NOT NULL,
-  `creditor_id` INT NOT NULL,
-  `debitor_account` VARCHAR(128) NOT NULL,
-  `creditor_account` VARCHAR(128) NOT NULL,
-  `status` TINYINT NOT NULL DEFAULT 0,
-  `created` TIMESTAMP NOT NULL DEFAULT NOW(),
-  `updated` TIMESTAMP NULL,
+  `id` INT NOT NULL AUTO_INCREMENT COMMENT 'auto-generated ID',
+  `token_id` INT NOT NULL COMMENT 'the token this clearing is for',
+  `debitor_id` INT NOT NULL COMMENT 'the debitor, i.e. the customer that owes to the creditor customer',
+  `creditor_id` INT NOT NULL COMMENT 'the creditor customer',
+  `debitor_account` VARCHAR(128) NOT NULL COMMENT 'debitor account information, arbitrary\nJSON data',
+  `creditor_account` VARCHAR(128) NOT NULL COMMENT 'creditor account information, arbitrary\nJSON data',
+  `status` TINYINT NOT NULL DEFAULT 0 COMMENT 'clearing status, updatable through clearing API',
+  `created` TIMESTAMP NOT NULL DEFAULT NOW() COMMENT 'time of creation (token completion)',
+  `updated` TIMESTAMP NULL COMMENT 'time of last modification',
   PRIMARY KEY (`id`),
   INDEX `fk_clearing_token1_idx` (`token_id` ASC),
   INDEX `fk_clearing_customer1_idx` (`debitor_id` ASC),
@@ -193,7 +199,8 @@ CREATE TABLE IF NOT EXISTS `clearing` (
     REFERENCES `customer` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+COMMENT = 'holds clearing data for completed tokens that need to be settled';
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
