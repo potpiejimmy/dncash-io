1a99ae4bd841b6767ddcbf587bba4a9d3e6ecce6
Thorsten Liese <thorsten.liese@dieboldnixdorf.com>
2018-05-07 17:41:14 +0200

    BCSDM-5: improved child process using spawn with inherited io.

--- awsdeploy.ts
+++ awsdeploy.ts
@@ -1,5 +1,3 @@
-import { exec } from "child_process";
+import { spawn } from "child_process";
 
-var pr = exec("eb deploy -l dncash-io-" + process.env.npm_package_version);
-pr.stdout.pipe(process.stdout);
-pr.stderr.pipe(process.stderr);
+spawn("eb", ["deploy","-l","dncash-io-"+process.env.npm_package_version], {stdio:'inherit'});
